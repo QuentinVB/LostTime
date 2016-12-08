9aebcff4f51ffbe585568c7dae9d02e1845b9a79
V. Amdidouche <amdidouche@intechinfo>
2016-12-08 10:33:06 +0100

    Player Movement workin, camera in progress

---
+++ Assets/Script/Player/PlayerMovementBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementBehavior : MonoBehaviour {
+    public VirtualLeftJoystick LeftJoystick;
+    public float MoveSpeed = 5.0f;
+    public float Drag = 0.5f;
+    public Vector3 MoveVector { get; set; }
+    private Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = gameObject.AddComponent<Rigidbody>();
+        playerRigidbody.drag = Drag;
+        playerRigidbody.mass = 50;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 movement = new Vector3();
+        movement.x = LeftJoystick.LeftVertical();
+        movement.z = LeftJoystick.LeftHorizontal();
+        transform.position +=  movement;
+
+        MoveVector = PoolInput();
+        Move();
+    }
+    private void Move()
+    {
+        playerRigidbody.AddForce(MoveVector * MoveSpeed);
+    }
+
+    private Vector3 PoolInput()
+    {
+        Vector3 movement = Vector3.zero;
+        movement.x = LeftJoystick.LeftHorizontal();
+        movement.z = movement.x = LeftJoystick.LeftVertical();
+
+        if(movement.magnitude > 1)
+        {
+            movement.Normalize();
+        }
+        return movement;
+    }
+}
