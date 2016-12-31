363830ff330dc5366be1c64223a555a52f67d6b6
QVanbutsele <vanbutsele@intechinfo.fr>
2016-12-31 12:27:56 +0100

    update UVAnimation ; set UVTileSelect system

--- Assets/Scripts/Animation/UVTileSelect.cs
+++ Assets/Scripts/Animation/UVTileSelect.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class UVAnimation : MonoBehaviour {
+/// <summary>
+/// Allow to chose a specified tile on the tileset based on the X/Y division of the tileset
+/// </summary>
+public class UVTileSelect : MonoBehaviour {
     public int UVTileX = 4;
     public int UVTileY = 4;
-    public int fps = 30;
     private int uIndex = 0;
     private int vIndex = 0;
-    private int index;
+    public int index = 1;
     private Vector2 size;
     private Vector2 offset;
     private Renderer renderer;
@@ -23,16 +24,15 @@ public class UVAnimation : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         //set index
-        index = (int)(Time.time * fps);
-
         //cut index based on the current frame from the begining
-        index = index % (UVTileX * UVTileY);  
+        
+        index = index % (UVTileX * UVTileY);
         uIndex = index % UVTileY;
         vIndex = index / UVTileX;
 
         offset = new Vector2(uIndex * size.x, 1.0f - size.y - vIndex * size.y);
 
         //offset the uv at the position and rescale based ont the size of the tile
-        renderer.material.SetTextureOffset("_MainTex", offset);
+        renderer.material.SetTextureOffset("_MainTex", offset);       
     }
 }
