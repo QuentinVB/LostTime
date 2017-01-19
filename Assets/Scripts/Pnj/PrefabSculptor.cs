d6db7f9fb3277dbbf335133e9c32111a159eb9bb
QVanbutsele <vanbutsele@intechinfo.fr>
2017-01-19 15:44:39 +0100

    set elementary instance ; test ok ! we need to talk !

--- Assets/Scripts/Pnj/PrefabSculptor.cs
+++ Assets/Scripts/Pnj/PrefabSculptor.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PrefabSculptor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    //public Transform cubetest;
+    public Transform characterLowPo;
+    public Object clone;
+    public GameObject cloned;
+
+    void Start()
+    {
+        //for (int y = 0; y < 5; y++)
+        //{
+        //    for (int x = 0; x < 5; x++)
+        //    {
+        //        Instantiate(cubetest, new Vector3(x, y, 0), Quaternion.identity);
+        //    }
+        //}
+        for (int i = 0; i < 3; i++)
+        {
+            clone = Instantiate(characterLowPo, new Vector3(i+0.8f, 0, 0), Quaternion.identity);
+            clone.name = i.ToString();        
+        }
+    }
+        // Update is called once per frame
+    void Update () 
+    {
 		
 	}
 }
