a881bdb971bfde34f6715eb726800cca1c5cd780
QVanbutsele <vanbutsele@intechinfo.fr>
2017-01-19 12:31:37 +0100

    clean Develop after merging ;

---
+++ Assets/Scripts/Camera/CameraPositionTemp.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPositionTemp : MonoBehaviour {
+
+    public VirtualRightJoystick rightJoystick;
+
+    private Vector3 initial;
+    private float rotationDelta;
+    private float translateDelta;
+    private float resetDelta;
+
+    private float inputH;
+    private float inputV;
+
+    // Use this for initialization
+    void Start () {
+        rotationDelta = 50;
+        translateDelta = 0.1f;
+        resetDelta = 0.1f;
+        initial = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InputH = rightJoystick.RightHorizontal();
+        InputV = rightJoystick.RightVertical();
+        Debug.Log(string.Format("{0},  {1}", InputH, InputV));
+
+        //rotation (left and right)
+        transform.RotateAround(transform.parent.position, Vector3.up, -InputH * rotationDelta * Time.deltaTime);
+        
+        //translation (up an down)
+        transform.Translate(Vector3.up* InputV * translateDelta, Space.World);
+
+        //reset position
+        if (InputV == 0 
+            && InputH == 0
+            && transform.localPosition != initial) transform.localPosition = Vector3.MoveTowards(transform.localPosition,initial, resetDelta);
+    }
+    public float InputH { get { return inputH; } set { inputH = Mathf.Clamp(value, -1.0f, 1.0f); } }
+    public float InputV { get { return inputV; } set { inputV = Mathf.Clamp(value, -1.0f, 1.0f); } }
+
+}
+//Vector3.down = rotation de la camera vers la droite
+//Vector3.up = rotation vers la gauche.
+//transform.RotateAround(Vector3.zero, Vector3.down, 30 * Time.deltaTime);
