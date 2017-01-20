9b83b717ea3ee22e09e9be42e82531c15950acad
QVanbutsele <vanbutsele@intechinfo.fr>
2017-01-20 15:52:12 +0100

    move assets3D to Resources (!) ; setup ISculptor ; writed HumanSculptor V1;

---
+++ Assets/Scripts/Pnj/HumanSculptor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+
+class HumanSculptor : MonoBehaviour,ISculptor
+{
+
+    private Object humanPrefab;
+    private Material defaultMaterial;
+    public HumanSculptor()
+    {
+        humanPrefab = Resources.Load("CharacterLowPo/CharacterLowPo");
+        defaultMaterial = (Material)Resources.Load("CharacterLowPo/CharacterLowPo/Materials/jazz");
+    }
+    public GameObject GetPrefab
+    {
+        get
+        {
+            return (GameObject)Instantiate(humanPrefab);
+        }
+    }
+    public GameObject PrefabByName(string characterType)
+    {    
+        GameObject humanToSculpt = (GameObject)Instantiate(humanPrefab);
+        humanToSculpt.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material = skinSwitch(characterType);
+        return humanToSculpt;
+    }
+    public GameObject PrefabByName(string name, Transform initialTransform)
+    {
+        return (GameObject)Instantiate(humanPrefab, initialTransform);
+    }
+    private Material skinSwitch(string characterType)
+    {
+        return defaultMaterial;
+    }
+}
--- Assets/Scripts/Pnj/PrefabSculptor.cs
+++ Assets/Scripts/Pnj/PrefabSculptor.cs
@@ -10,8 +10,12 @@ public class PrefabSculptor : MonoBehaviour {
     public Object clone;
     public GameObject cloned;
 
+    Object myPrefab;
+    
+
     void Start()
     {
+        myPrefab = Resources.Load("CharacterLowPo/CharacterLowPo");
         //for (int y = 0; y < 5; y++)
         //{
         //    for (int x = 0; x < 5; x++)
@@ -21,8 +25,10 @@ public class PrefabSculptor : MonoBehaviour {
         //}
         for (int i = 0; i < 3; i++)
         {
-            clone = Instantiate(characterLowPo, new Vector3(i+0.8f, 0, 0), Quaternion.identity);
-            clone.name = i.ToString();        
+            clone = Instantiate(myPrefab, new Vector3(i + 0.8f, 0, 0), Quaternion.identity);
+            clone.name = i.ToString();
+            cloned = GameObject.Find(i.ToString());
+            cloned.GetComponent<Transform>().Translate(0, 1.0f + i, 0);
         }
     }
         // Update is called once per frame
