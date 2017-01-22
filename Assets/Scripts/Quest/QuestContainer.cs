7dfbba3e435e7ee0b312af5631128b3effdba3b2
Shadownsf <kore@intechinfo.fr>
2017-01-22 17:06:57 +0100

    Event , QuestManager  ( squelette + processEvent)  , QuestContainer.( without zenject)

---
+++ Assets/Scripts/Quest/QuestContainer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+
+public class QuestContainer : MonoBehaviour
+{
+
+    [XmlRoot("QuestContainer")]
+    public class QuestContainer
+    {
+        [XmlArray("QuestCollection")]
+        [XmlArrayItem("Quest")]
+        public List<Quest> questCollection = new List<Quest>();
+        public string nameOfcurrentQuest;
+    }
+
+    public class Quest
+    {
+
+        [XmlAttribute("valuesList")]
+        string valuesList;
+        [XmlAttribute("currentState")]
+        string currentState;
+        [XmlAttribute("currentActivity")]
+        string currentActivity;
+        [XmlAttribute("questDescritpionID")]
+        int questDescritpionID;
+        [XmlAttribute("questID")]
+        int questID;
+
+        [XmlAttribute("nameOfQuest")]
+        List<LinkedActor> LinkedActorList = new List<LinkedActor>();
+        [XmlArray("StateArray")]
+        [XmlArrayItem("State")]
+        State[] StateArray;
+
+
+    }
+
+    public class LinkedActor
+    {
+
+        string id;
+        string name;
+        string job;
+    }
+
+
+    public class State
+    {
+        bool isSetUp { get; set; }
+        List<Actor> actor = new List<Actor>();
+
+        bool get()
+        {
+            return this.isSetUp;
+        }
+        void set()
+        {
+            this.isSetUp = true;
+        }
+    }
+
+    class Actor
+    {
+        string id;
+        bool isSetUp { get; set; }
+        void Update()
+        {
+        }
+
+        List<IEvent> actionListSetUp = new List<IEvent>(); //  ??
+        List<IEvent> actionListActive = new List<IEvent>(); // ??
+        IEvent currentAction;
+        bool get()
+        {
+            return this.isSetUp;
+        }
+        void set()
+        {
+            this.isSetUp = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class QuestManager : MonoBehaviour
+{
+    private void Start()
+    {
+        setUP();
+    }
+
+    void editValue(IEvent Event)
+    {
+
+    }
+
+    void dialogue(IEvent Event)
+    {
+
+    }
+    void addNPC(IEvent Event)
+    {
+
+    }
+    void switchState(IEvent Event)
+    {
+
+    }
+
+    void setUP()
+    {
+
+    }
+
+    public void ProcessEvent(string functionName, IEvent Event)
+    {
+        switch (functionName)
+        {
+            case "editValue":
+                editValue(Event);
+                break;
+            case "dialogue":
+                dialogue(Event);
+                break;
+            case "addNPC":
+                addNPC(Event);
+                break;
+            case "switchState":
+                switchState(Event);
+                break;
+            default:
+                Debug.Log("Bad name of fuction");
+                break;
+        }
+    }
+}
