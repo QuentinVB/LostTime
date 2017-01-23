d66c6b2991b6b5d846342f28d30a6eaf851cb91d
QVanbutsele <vanbutsele@intechinfo.fr>
2017-01-23 16:45:06 +0100

    soften night and day

--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -20,6 +20,7 @@ public class Timer : MonoBehaviour {
         sun = GameObject.Find("Sun").GetComponent<Light>();
         sunInitialIntensity = sun.intensity;
         jeuFini = false;
+
         swapMalus = 0;
         level = "LostTimeGearDistrict";
         getSwapMalus();
@@ -55,12 +56,12 @@ public class Timer : MonoBehaviour {
         sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
         float intensityMultiplier = 1;
 
-        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75)
-            intensityMultiplier = 0f;
-        else if (currentTimeOfDay <= 0.25f)
-            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 23f) * (1 / 0.02f));
-        else if (currentTimeOfDay >= 0.83)
-            intensityMultiplier = Mathf.Clamp01(1 - (currentTimeOfDay - 83f) * (1 / 0.02f));
+        //if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75)
+        //    intensityMultiplier = 0f;
+        //else if (currentTimeOfDay <= 0.25f)
+        //    intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 23f) * (1 / 0.02f));
+        //else if (currentTimeOfDay >= 0.83)
+        //    intensityMultiplier = Mathf.Clamp01(1 - (currentTimeOfDay - 83f) * (1 / 0.02f));
 
         sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
