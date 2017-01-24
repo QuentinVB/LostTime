a0c820b34406371906ceb112ea033d31eaee76e9
QVanbutsele <vanbutsele@intechinfo.fr>
2017-01-24 16:32:53 +0100

    add clouds and fog above the city; maybe the same system for  the menu \o/

---
+++ Assets/Scripts/Decor/CloudAbove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudAbove : MonoBehaviour {
+    private Renderer render;
+    public float scrollSpeed;
+
+    // Use this for initialization
+    void Start () {
+        render = GetComponent<Renderer>();
+        scrollSpeed = 0.1f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * scrollSpeed;
+        render.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
